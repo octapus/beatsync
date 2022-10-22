cf283061c837980a094e8f294a6894f755ebf236
octapus <24534282+octapus@users.noreply.github.com>
2022-10-22 19:54:28 -0400

    Read file from cmdline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{env, path::Path};
+
+const USAGE: &str = "Usage: beatsync <file.wav>";
+
 fn main() {
-	println!("Hello, world!");
+	// Can be changed to let file ... else {} once let else is stable
+	let file = match env::args().nth(1) {
+		Some(f) => f,
+		None => {
+			println!("{}", USAGE);
+			return;
+		}
+	};
+	let file = Path::new(&file);
+	if !file.is_file() {
+		println!("{}", USAGE);
+		return;
+	}
+	println!("File: {:?}", file)
 }
