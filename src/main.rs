a811270c1a2deba0595e7a581cc00a2bc257e0d0
octapus <24534282+octapus@users.noreply.github.com>
2022-10-22 20:52:35 -0400

    Add basic wav read with hound

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-use std::{env, path::Path};
+use std::env;
 
 const USAGE: &str = "Usage: beatsync <file.wav>";
 
 fn main() {
-	// Can be changed to let file ... else {} once let else is stable
-	let file = match env::args().nth(1) {
+	// waiting for if let chains to become stable...
+	let file = match env::args_os().nth(1) {
 		Some(f) => f,
 		None => {
 			println!("{}", USAGE);
 			return;
 		}
 	};
-	let file = Path::new(&file);
-	if !file.is_file() {
-		println!("{}", USAGE);
-		return;
+	let mut reader = match hound::WavReader::open(file) {
+		Ok(r) => r,
+		Err(_) => {
+			println!("{}", USAGE);
+			return;
+		}
+	};
+
+	assert_eq!(2, reader.spec().channels);
+	assert_eq!(16, reader.spec().bits_per_sample);
+	assert_eq!(hound::SampleFormat::Int, reader.spec().sample_format);
+
+	for sample in reader.samples::<i16>().skip(200).take(20) {
+		println!("{}", sample.unwrap());
 	}
-	println!("File: {:?}", file)
 }
