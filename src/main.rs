40be6cbf4193a43549a7777bc34a0c2ef28558b6
octapus <24534282+octapus@users.noreply.github.com>
2022-10-23 15:53:37 -0400

    Add basic window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 
+use minifb::{Window, WindowOptions, Key};
+
 const USAGE: &str = "Usage: beatsync <file.wav>";
+
+const WIDTH: usize = 1920;
+const HEIGHT: usize = 1080;
 
 fn main() {
 	// waiting for if let chains to become stable...
@@ -25,5 +30,23 @@ fn main() {
 
 	for sample in reader.samples::<i16>().skip(200).take(20) {
 		println!("{}", sample.unwrap());
+	}
+
+
+	// gui stuff
+	// in future, make this buffer mut and update it in main loop
+	let buffer: Vec<u32> = vec![u32::MAX; WIDTH * HEIGHT];
+	let mut window = Window::new(
+		"Test - ESC to exit",
+		WIDTH,
+		HEIGHT,
+		WindowOptions::default()
+	).unwrap_or_else(|e| { panic!("{}", e); });
+	// 16700 for 60 fps, 6900 for 144
+	window.limit_update_rate(Some(std::time::Duration::from_micros(6800)));
+
+	// main loop
+	while window.is_open() && !window.is_key_down(Key::Escape) && !window.is_key_down(Key::Q) {
+		window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
 	}
 }
